8a726682a34750e8f8abf63b0d43628001ad6357
Manana <manana.lortkipanidze@epfl.ch>
2018-12-21 18:24:30 +0100

    small bug fixed

--- assets/scripts/world_map.js
+++ assets/scripts/world_map.js
@@ -38,70 +38,71 @@ var tooltip_map = d3.select("body")
   .attr("class", "tooltip")
   .style("opacity", 0);
 
+/*reading news data file with links to articles*/
+d3.csv("assets/data/wiki_news.csv").then(function(news) {
+  /* reading json file containing world map in geojson format*/
+  d3.json("assets/data/world.geo.json")
+    .then(function(map_data) {
 
-/* reading json file containing world map in geojson format*/
-d3.json("assets/data/world.geo.json")
-  .then(function(map_data) {
+      /*linking map with world geojson, entering data*/
+      g.selectAll("path")
+        .data(map_data.features)
+        .enter()
+        .append("path")
+        .attr("class", "country") /*class for styling and accessing*/
+        .attr("d", path)
+        .style("stroke", "white")
+        .style('fill', function(d) {
+          return "#F2D165";
+        })
+        .on("mouseover", function() {
+          d3.select(this).transition()
+            .duration("100")
+            .style("fill-opacity", ".8");
 
-    /*linking map with world geojson, entering data*/
-    g.selectAll("path")
-      .data(map_data.features)
-      .enter()
-      .append("path")
-      .attr("class", "country") /*class for styling and accessing*/
-      .attr("d", path)
-      .style("stroke", "white")
-      .style('fill', function(d) {
-        return "#F2D165";
-      })
-      .on("mouseover", function() {
-        d3.select(this).transition()
-          .duration("100")
-          .style("fill-opacity", ".8");
+          /*Show tooltip_map*/
+          tooltip_map.transition()
+            .duration(200)
+            .style("opacity", .9);
 
-        /*Show tooltip_map*/
-        tooltip_map.transition()
-          .duration(200)
-          .style("opacity", .9);
+        })
+        .on("mousemove", function(d) {
+          /*Place the tooltip_map*/
+          tooltip_map.style("left", (d3.mouse(this)[0]) + offsetL + "px")
+            .style("top", d3.event.pageY + "px");
 
-      })
-      .on("mousemove", function(d) {
-        /*Place the tooltip_map*/
-        tooltip_map.style("left", (d3.mouse(this)[0]) + offsetL + "px")
-          .style("top", d3.event.pageY + "px");
+          /*display country name as the header of tooltip content*/
+          var toolTipHtml = "<h3 id='toolTipHeader'>Country: " + d.properties.admin + "</h3>";
 
-        /*display country name as the header of tooltip content*/
-        var toolTipHtml = "<h3 id='toolTipHeader'>Country: " + d.properties.admin + "</h3>";
+          /*in case country contains news (hence the color) for given circumstences, display news*/
+          if (d3.select(this).style('fill') == "rgb(236, 81, 72)") {
+            toolTipHtml = toolTipHtml + getNewsOfTheCountry(news, d);
+          } else {
+            /*otherwise display information regarding the country*/
+            toolTipHtml += "<p class='toolTipText'>Continent: " + d.properties.continent + "</p>" +
+              "<p class='toolTipText'>Economy: " + d.properties.economy + "</p>" +
+              "<p class='toolTipText'>Formal: " + d.properties.formal_en + "</p>" +
+              "<p class='toolTipText'>Region: " + d.properties.region_wb + "</p>";
+          }
 
-        /*in case country contains news (hence the color) for given circumstences, display news*/
-        if (d3.select(this).style('fill') == "rgb(236, 81, 72)") {
-          toolTipHtml = toolTipHtml + getNewsOfTheCountry(news, d);
-        } else {
-          /*otherwise display information regarding the country*/
-          toolTipHtml += "<p class='toolTipText'>Continent: " + d.properties.continent + "</p>" +
-            "<p class='toolTipText'>Economy: " + d.properties.economy + "</p>" +
-            "<p class='toolTipText'>Formal: " + d.properties.formal_en + "</p>" +
-            "<p class='toolTipText'>Region: " + d.properties.region_wb + "</p>";
-        }
+          tooltip_map.html(toolTipHtml);
 
-        tooltip_map.html(toolTipHtml);
+        })
+        .on("mouseout", function() {
+          tooltip_map.transition()
+            .duration(200)
+            .style("opacity", 0);
+          d3.select(this)
+            .transition()
+            .duration("100")
+            .style("fill-opacity", "1")
+        })
 
-      })
-      .on("mouseout", function() {
-        tooltip_map.transition()
-          .duration(200)
-          .style("opacity", 0);
-        d3.select(this)
-          .transition()
-          .duration("100")
-          .style("fill-opacity", "1")
-      })
+      /* color countries with news*/
+      colorMap();
 
-    /* color countries with news*/
-    colorMap();
-
-  });
-
+    });
+});
 
 
 /*returns color of the country corresponding to the data d, given selected article in network
